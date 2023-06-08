f74bfde5302a6a9e18cdc833b17168c1b8e393a3
Kashif Jilani <kashifjilani@Kashifs-MacBook-Pro.local>
2023-06-08 16:15:43 +0530

    Changes for 02_pipeline on main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier import logger
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -7,7 +9,18 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=======")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f"***************************************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=======")
 except Exception as e:
     logger.exception(e)
     raise e
